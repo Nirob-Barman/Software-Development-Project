9df2ad7658c2d40bd9325dea759a29464f149856
Nirob-Barman <niroborin111@gmail.com>
2023-12-19 23:12:15 +0600

    Practicing

--- Module 20/Car_Hub_Market/Car_Hub_Market/settings.py
+++ Module 20/Car_Hub_Market/Car_Hub_Market/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django
 print(django.get_version())
@@ -137,6 +138,7 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 # MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
