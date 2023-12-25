e9c8d90f956183d9e52dcd7ec0068f8d37e3519e
Nirob-Barman <niroborin111@gmail.com>
2023-12-25 08:14:31 +0600

    Deposite view

--- Module 21/mamar_bank/transactions/views.py
+++ Module 21/mamar_bank/transactions/views.py
@@ -40,3 +40,33 @@ class TransactionCreateMixin(LoginRequiredMixin, CreateView):
         })
 
         return context
+
+
+class DepositMoneyView(TransactionCreateMixin):
+    form_class = DepositForm
+    title = 'Deposit'
+
+    def get_initial(self):
+        initial = {'transaction_type': DEPOSIT}
+        return initial
+
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        # if not account.initial_deposit_date:
+        #     now = timezone.now()
+        #     account.initial_deposit_date = now
+        
+        account.balance += amount
+        account.save(
+            update_fields=[
+                'balance'
+            ]
+        )
+
+        messages.success(
+            self.request,
+            f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully'
+        )
+
+        return super().form_valid(form)
