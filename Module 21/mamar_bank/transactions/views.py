dc093f27df9983c68886146b30ecde2a7b5d80f8
Nirob-Barman <niroborin111@gmail.com>
2023-12-25 09:40:34 +0600

    Payloan LoanListView Urls Transaction

--- Module 21/mamar_bank/transactions/views.py
+++ Module 21/mamar_bank/transactions/views.py
@@ -108,11 +108,89 @@ class LoanRequestView(TransactionCreateMixin):
         amount = form.cleaned_data.get('amount')
         current_loan_count = Transaction.objects.filter(
             account=self.request.user.account, transaction_type=3, loan_approve=True).count()
+        
         if current_loan_count >= 3:
             return HttpResponse("You have cross the loan limits")
+        
         messages.success(
             self.request,
             f'Loan request for {"{:,.2f}".format(float(amount))}$ submitted successfully'
         )
 
         return super().form_valid(form)
+
+
+class TransactionReportView(LoginRequiredMixin, ListView):
+    template_name = 'transactions/transaction_report.html'
+    model = Transaction
+    # display the total balance after filter the data based on start_date and end_date
+    balance = 0 
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(
+            account=self.request.user.account
+        )
+        start_date_str = self.request.GET.get('start_date')
+        end_date_str = self.request.GET.get('end_date')
+
+        if start_date_str and end_date_str:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+
+            queryset = queryset.filter(
+                timestamp__date__gte=start_date, timestamp__date__lte=end_date)
+            self.balance = Transaction.objects.filter(
+                timestamp__date__gte=start_date, timestamp__date__lte=end_date
+            ).aggregate(Sum('amount'))['amount__sum']
+        else:
+            self.balance = self.request.user.account.balance
+
+        # must return unique queryset
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'account': self.request.user.account
+        })
+
+        return context
+
+
+class PayLoanView(LoginRequiredMixin, View):
+    def get(self, request, loan_id):
+        loan = get_object_or_404(Transaction, id=loan_id)
+        print(loan)
+        if loan.loan_approve:
+            user_account = loan.account
+            # Reduce the loan amount from the user's balance
+            # 5000, 500 + 5000 = 5500
+            # balance = 3000, loan = 5000
+            if loan.amount < user_account.balance:
+                user_account.balance -= loan.amount
+                loan.balance_after_transaction = user_account.balance
+                user_account.save()
+                loan.loan_approved = True
+                loan.transaction_type = LOAN_PAID
+                loan.save()
+                return redirect('transactions:loan_list')
+            else:
+                messages.error(
+                    self.request,
+                    f'Loan amount is greater than available balance'
+                )
+
+        return redirect('loan_list')
+
+
+class LoanListView(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = 'transactions/loan_request.html'
+    context_object_name = 'loans'  # loan list ta ei loans context er moddhe thakbe
+
+    def get_queryset(self):
+        user_account = self.request.user.account
+        queryset = Transaction.objects.filter(
+            account=user_account, transaction_type=3)
+        print(queryset)
+        return queryset
